2c6874c82726438ac7f017f35c6cf62168eb88d6
Mitch Dunn <bmu7wk@virginia.edu>
2023-07-22 11:14:12 -0400

    LTI-121 Added DB query for checking if a section has already been waitlisted or not, added this and other waitlist info to the html pages

--- src/main/java/edu/virginia/its/canvas/roster/RosterManagementApplication.java
+++ src/main/java/edu/virginia/its/canvas/roster/RosterManagementApplication.java
@@ -2,8 +2,12 @@ package edu.virginia.its.canvas.roster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("edu.virginia.its.canvas.roster.repos")
+@EntityScan("edu.virginia.its.canvas.roster.model")
 public class RosterManagementApplication {
 
   public static void main(String[] args) {
