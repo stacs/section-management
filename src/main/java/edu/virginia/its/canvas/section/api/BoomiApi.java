615d28d6cc4f6c19245d7bfba933d6917f0d0568
Mitch Dunn <bmu7wk@virginia.edu>
2024-08-13 14:11:39 -0400

    LTI-273 If the number of sections to send to Boomi is over a certain amount (currently 50) then we split that request into multiples so we don't overwhelm Boomi

--- src/main/java/edu/virginia/its/canvas/section/api/BoomiApi.java
+++ src/main/java/edu/virginia/its/canvas/section/api/BoomiApi.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Lists;
 import edu.virginia.its.canvas.section.model.BoomiRequests.CanvasWaitlist;
 import edu.virginia.its.canvas.section.model.BoomiRequests.CanvasWaitlistStatus;
 import edu.virginia.its.canvas.section.model.BoomiResponses.SisSection;
@@ -32,6 +33,8 @@ public class BoomiApi {
   @Value("${ltitool.boomi.apiToken}")
   private String boomiApiToken;
 
+  private final Integer maxSectionsPerRequest;
+
   private final WebClient boomiApi;
   private final Duration requestTimeout;
   private final ObjectMapper objectMapper;
@@ -39,7 +42,9 @@ public class BoomiApi {
   @Autowired
   public BoomiApi(
       @Value("${ltitool.boomi.url}") String boomiUrl,
-      @Value("${ltitool.boomi.apiTimeout:15}") Integer boomiApiTimeout) {
+      @Value("${ltitool.boomi.apiTimeout:15}") Integer boomiApiTimeout,
+      @Value("${ltitool.boomi.maxSectionsPerRequest:50}") Integer maxSectionsPerRequest) {
+    this.maxSectionsPerRequest = maxSectionsPerRequest;
     boomiApi = WebClient.create(boomiUrl);
     requestTimeout = Duration.ofSeconds(boomiApiTimeout);
     objectMapper = new ObjectMapper();
@@ -47,68 +52,77 @@ public class BoomiApi {
     objectMapper.configOverride(Boolean.class).setFormat(JsonFormat.Value.forShape(Shape.STRING));
     log.info("Boomi API URL: {}", boomiUrl);
     log.info("Boomi API Timeout: {}", boomiApiTimeout);
+    log.info("Boomi Max Sections Per Request: {}", maxSectionsPerRequest);
   }
 
   public List<SisSection> getWaitlistStatusForSections(List<CanvasWaitlistStatus> sections) {
+    List<SisSection> sisSections = new ArrayList<>();
     if (!sections.isEmpty()) {
-      ObjectNode body = objectMapper.createObjectNode();
-      ArrayNode waitlists = objectMapper.valueToTree(sections);
-      body.set("classes", waitlists);
-      return boomiApi
-          .post()
-          .uri("ws/rest/uvacanvas/canvasWaitlistStatus")
-          .header("X-API-KEY", boomiApiToken)
-          .contentType(MediaType.APPLICATION_JSON)
-          .bodyValue(body)
-          .retrieve()
-          .bodyToMono(JsonNode.class)
-          .map(s -> s.path("waitlists"))
-          .map(
-              s -> {
-                try {
-                  return new ObjectMapper()
-                      .readValue(s.traverse(), new TypeReference<List<SisSection>>() {});
-                } catch (IOException e) {
-                  log.error("Error while deserializing waitlist sections json", e);
-                  return new ArrayList<SisSection>();
-                }
-              })
-          .block(requestTimeout);
-    } else {
-      return List.of();
+      List<List<CanvasWaitlistStatus>> sectionsList =
+          Lists.partition(sections, maxSectionsPerRequest);
+      for (List<CanvasWaitlistStatus> canvasWaitlistStatuses : sectionsList) {
+        ObjectNode body = objectMapper.createObjectNode();
+        ArrayNode waitlists = objectMapper.valueToTree(canvasWaitlistStatuses);
+        body.set("classes", waitlists);
+        List<SisSection> response =
+            boomiApi
+                .post()
+                .uri("ws/rest/uvacanvas/canvasWaitlistStatus")
+                .header("X-API-KEY", boomiApiToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .map(s -> s.path("waitlists"))
+                .map(
+                    s -> {
+                      try {
+                        return new ObjectMapper()
+                            .readValue(s.traverse(), new TypeReference<List<SisSection>>() {});
+                      } catch (IOException e) {
+                        log.error("Error while deserializing waitlist sections json", e);
+                        return new ArrayList<SisSection>();
+                      }
+                    })
+                .block(requestTimeout);
+        if (response != null) {
+          sisSections.addAll(response);
+        }
+      }
     }
+    return sisSections;
   }
 
-  public boolean updateWaitlistsForSections(List<CanvasWaitlist> sections) {
+  public void updateWaitlistsForSections(List<CanvasWaitlist> sections) {
     if (!sections.isEmpty()) {
-      ObjectNode body = objectMapper.createObjectNode();
-      ArrayNode waitlists = objectMapper.valueToTree(sections);
-      body.set("waitlists", waitlists);
-      ClientResponse response =
-          boomiApi
-              .post()
-              .uri("ws/rest/uvacanvas/canvasWaitlist")
-              .header("X-API-KEY", boomiApiToken)
-              .contentType(MediaType.APPLICATION_JSON)
-              .bodyValue(body)
-              .exchange()
-              .block(requestTimeout);
-      if (response != null) {
-        if (HttpStatus.OK.equals(response.statusCode())) {
-          return true;
+      List<List<CanvasWaitlist>> sectionsList = Lists.partition(sections, maxSectionsPerRequest);
+      for (List<CanvasWaitlist> canvasWaitlists : sectionsList) {
+        ObjectNode body = objectMapper.createObjectNode();
+        ArrayNode waitlists = objectMapper.valueToTree(canvasWaitlists);
+        body.set("waitlists", waitlists);
+        ClientResponse response =
+            boomiApi
+                .post()
+                .uri("ws/rest/uvacanvas/canvasWaitlist")
+                .header("X-API-KEY", boomiApiToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .exchange()
+                .block(requestTimeout);
+        if (response != null) {
+          if (!HttpStatus.OK.equals(response.statusCode())) {
+            log.error(
+                "Received a '{}' response from server with JSON '{}' when attempting to set SIS waitlisted sections",
+                response.statusCode(),
+                response.bodyToMono(String.class));
+          }
         } else {
           log.error(
-              "Received a '{}' response from server with JSON '{}' when attempting to set SIS waitlisted sections",
-              response.statusCode(),
-              response.bodyToMono(String.class));
+              "Received no response from server when attempting to set SIS waitlisted sections");
         }
-      } else {
-        log.error(
-            "Received no response from server when attempting to set SIS waitlisted sections");
       }
     } else {
       log.warn("Waitlist sections list to send to SIS was empty");
     }
-    return false;
   }
 }
